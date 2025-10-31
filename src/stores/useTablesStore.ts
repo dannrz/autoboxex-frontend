f55ff6415f0caa058f8d576e8b8eaa9be4b0db43
Daniel <rozd.21d97@gmail.com>
2025-10-30 18:01:00 -0600

    feat: agregar Dockerfile y configuración de Nginx para el despliegue de la aplicación

--- src/stores/useTablesStore.ts
+++ src/stores/useTablesStore.ts
@@ -1,10 +1,10 @@
-import type { Costos, Insumos, Precios } from "@/modules/register/interfaces";
+import type { Costos, Insumo, Precios } from "@/modules/register/interfaces";
 import { defineStore } from "pinia";
 
 export const useTablesStore = defineStore('tables', {
     state: () => ({
         costos: [] as Costos[],
-        insumos: [] as Insumos[],
+        insumos: [] as Insumo[],
         precios: [] as Precios[],
     })
 });
