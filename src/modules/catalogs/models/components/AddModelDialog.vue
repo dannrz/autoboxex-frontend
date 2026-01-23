d3a8aa585898adcd9e0977a0aa5f0a2caaea7622
Daniel <rozd.21d97@gmail.com>
2026-01-22 18:27:03 -0600

    feat: mejorar gestión de modelos con optimización en el diálogo y la tabla, incluyendo estado de carga

--- src/modules/catalogs/models/components/AddModelDialog.vue
+++ src/modules/catalogs/models/components/AddModelDialog.vue
@@ -3,35 +3,37 @@
         <div class="flex flex-col gap-6">
             <FloatLabel variant="on" class="col-span-3">
                 <Select id="marca" class="w-full" size="small" :options="brands" optionLabel="Marca" filter
-                    v-model="model!.Marca" />
+                    v-model="model.Marca" />
                 <label for="marca">Marca</label>
             </FloatLabel>
             <FloatLabel variant="on" class="col-span-3">
-                <InputText id="modelo" class="w-full" size="small" v-model="model!.Modelo" />
+                <InputText id="modelo" class="w-full" size="small" v-model="model.Modelo" />
                 <label for="modelo">Modelo</label>
             </FloatLabel>
         </div>
         <template #footer>
             <Button label="Cancelar" icon="pi pi-times" text @click="$emit('update:dialog', false)" />
-            <Button label="Guardar" icon="pi pi-save" @click="saveData(model)" />
+            <Button v-if="!isLoadingSaveModel" label="Guardar" icon="pi pi-save" @click="saveData" />
+            <Button v-else class="w-[8em]" disabled outlined>
+                <ProgressSpinner id="spinner" />
+            </Button>
         </template>
     </Dialog>
 </template>
 
 <script setup lang="ts">
 import type { Brand } from '../../brands/interfaces';
-import { useModels } from '../composables/useModels';
 import type { ModelRequest } from '../interfaces';
 
 defineProps<{
     dialog: boolean,
     brands: Array<Brand>,
-    saveData: (data: ModelRequest) => void
+    isLoadingSaveModel: boolean,
+    saveData: () => void,
+    model: ModelRequest,
 }>();
 
 defineEmits<{
     'update:dialog': [value: boolean],
 }>();
-
-const { model } = useModels();
 </script>
