bdc41e484a7aa53e8b3a563365a89b4afd00f01f
Daniel <rozd.21d97@gmail.com>
2026-01-29 14:01:58 -0600

    feat: agregar gestión de refacciones con tabla, servicio y vista

---
+++ src/modules/catalogs/refacs/components/RefacsTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <DataTable v-model:filters="$props.filters" :value="refacciones" paginator :rows="10"
+        :loading="$props.refacciones.length === 0" responsiveLayout="scroll" :rowsPerPageOptions="[10, 20, 50, 100]"
+        :globalFilterFields="['Refacción', 'Codigo']">
+        <template #header>
+            <div class="flex justify-end">
+                <IconField>
+                    <InputIcon>
+                        <i class="pi pi-search" />
+                    </InputIcon>
+                    <InputText v-model="filters['global'].value" placeholder="Buscar..." />
+                </IconField>
+            </div>
+        </template>
+        <Column field="IdRefaccion" header="ID" />
+        <Column field="Refacción" header="Refacción" />
+        <Column field="Unidad" header="Unidad" />
+        <Column field="Codigo" header="Código" />
+        <Column field="Tipo" header="Tipo" />
+        <Column field="Cantidad" header="Cantidad" />
+        <Column field="Precio" header="Precio" />
+        <Column field="Fecha" header="Fecha" />
+        <Column field="Marca" header="Marca" />
+        <Column field="Calidad" header="Calidad" />
+        <Column field="PrecioIva" header="Precio con IVA" />
+    </DataTable>
+</template>
+
+<script setup lang="ts">
+import type { Refaccion } from '../interfaces';
+
+defineProps<{
+    refacciones: Array<Refaccion>,
+    filters: any,
+}>();
+</script>
