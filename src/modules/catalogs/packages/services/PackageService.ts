b5d524a7552fba49782a3d64c9e9079cb8ce4a99
Daniel <rozd.21d97@gmail.com>
2026-03-18 18:02:24 -0600

    feat: implementar gestión de paquetes con tabla, servicio y vista

--- src/modules/catalogs/packages/services/PackageService.ts
+++ src/modules/catalogs/packages/services/PackageService.ts
@@ -1,8 +1,9 @@
 import { api } from "@/api/baseApi";
 import type { AxiosResponse } from "axios";
+import type { Package } from "../interfaces";
 
 export const PackageService = {
-    async getPackages(): Promise<AxiosResponse> {
-        return await api.get("/packages");
+    async getPackages(): Promise<AxiosResponse<Array<Package>>> {
+        return await api.get<Array<Package>>("/catalogs/packages");
     },
 }
--- src/modules/catalogs/packages/views/PackageView.vue
+++ src/modules/catalogs/packages/views/PackageView.vue
@@ -2,10 +2,17 @@
     <h1 class="text-2xl text-center text-cyan-600">
         Paquetes
     </h1>
-    <PackagesTable />
+    <PackagesTable :packages :isLoadingPackages />
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { PackagesTable } from '../components';
+import { usePackage } from '../composables/usePackage';
 
+const { initData, packages, isLoadingPackages } = usePackage();
+
+onMounted(() => {
+    initData();
+});
 </script>
